087658a4f1b5303bfda196c5f4cfce16356a73da
coder1234567891011 <wesyoung98@gmail.com>
2023-09-22 12:32:39 -0400

    Adding Routes

--- Budget-App/src/app/app-routing.module.ts
+++ Budget-App/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { BudgetsComponent } from './components/budgets/budgets.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
+import { TransactionsComponent } from './components/transactions/transactions.component';
 
 const routes: Routes = [
-  {path:'app-home' , component: HomeComponent}
+  {path:'app-home' , component: HomeComponent},
+  {path:'app-profile' , component: ProfileComponent},
+  {path:'app-accounts' , component: AccountsComponent},
+  {path:'app-budgets' , component: BudgetsComponent},
+  {path:'app-transactions' , component: TransactionsComponent}
 ];
 
 @NgModule({
